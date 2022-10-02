8cdc9c76c4e762451ff48c9abd0c1dd030304637
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2022-10-02 15:06:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictonary = {
+  "😃": "Happy",
+  "💁": "What",
+  "👌": "Wonderfull",
+  "🎍": "Vegeies",
+  "😍": "Lovely",
+  "📙": "Book"
+};
+
+var emojiknown = Object.keys(emojiDictonary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictonary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "Sorry! We don't have this in our database.";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictonary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Inside Outt!</h1>
+      <input onChange={emojiInputHandler}></input>
+      <h2> {meaning}</h2>
+      <h3>Emojis we know</h3>
+      {emojiknown.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "1.5rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
